1dd4ee208e6d41c7179e8643c81dd2fd2d4a97f1
brankobjelic <33072204+brankobjelic@users.noreply.github.com>
2023-08-16 02:20:29 +0200

    added loading spinner

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function spinner() {
+    document.getElementsByClassName("loader")[0].style.display = "block";
+}
